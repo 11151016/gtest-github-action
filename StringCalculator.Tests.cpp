f42e184b310f8b4f95ffc56b6118cc1fa7b66daf
11151016 <46897277+11151016@users.noreply.github.com>
2024-08-20 15:48:59 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -11,3 +11,14 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_empty_strin
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
+
+TEST(string_calculator_add_when_passed_a_single_number,returns_sum_Val_for_empty_string){
+ //Arrange
+  StringCalculator calculator;
+  string input="12";
+  int expectedsum=3;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
